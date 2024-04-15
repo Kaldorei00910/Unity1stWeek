66bfc6feb31eff942980ac532e9677422f66f9be
LeeHwangChoong <lyd8224@gmail.com>
2024-04-15 15:22:45 +0900

    Update GameManager.cs

--- FindMemberGame/Assets/Scripts/GameManager.cs
+++ FindMemberGame/Assets/Scripts/GameManager.cs
@@ -8,14 +8,12 @@ public class GameManager : MonoBehaviour
 {
     public Text timeTxt;
     float time = 0.00f;
-
-    // Start is called before the first frame update
+        
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+        
     void Update()
     {
         time += Time.deltaTime;
