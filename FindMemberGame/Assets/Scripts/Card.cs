a96369d767bff22086bea02d9223a9a39d972698
Kaldorei00910 <117884183+Kaldorei00910@users.noreply.github.com>
2024-04-15 16:57:52 +0900

    1차완성본
    카드 판정 부분까지
    (게임끝내기 부분은X)

---
+++ FindMemberGame/Assets/Scripts/Card.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+
+    public Animator anim;
+
+    public SpriteRenderer frontImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Setting(int number) 
+    {
+        idx = number;
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}");
+    }   
+
+    public void OpenCard()
+    {
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
+
+        if (GameManager.instance.firstCard == null)
+        {
+            GameManager.instance.firstCard = this;
+        }
+        else
+        {
+            GameManager.instance.secondCard = this;
+            GameManager.instance.Matched();
+        }
+    }
+
+    public void DestroyCard()
+    {
+        Invoke("DestroyCardInvoke",1.0f);
+    }
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke",1.0f);
+    }
+
+    void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+}
---
+++ FindMemberGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Card firstCard;
+    public Card secondCard;
+
+    public Text timeTxt;
+    float time = 0.0f;
+
+    private void Awake()
+    {
+        if (instance == null)   
+            instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+    }
+    public void Matched()
+    {
+        if(firstCard.idx == secondCard.idx)
+        {
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+        }
+        else
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+        firstCard = null;
+        secondCard = null;
+    }
+
+}
